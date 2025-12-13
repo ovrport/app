99c89893485518e754bba7b1b5b08d0c4d18943b
Mathew Tkachuk <me@crx.moe>
2025-12-13 18:21:56 +0300

    Make bitmap mutable in BitmapIconResizer

--- composeApp/src/androidMain/kotlin/moe/crx/overport/app/BitmapIconResizer.kt
+++ composeApp/src/androidMain/kotlin/moe/crx/overport/app/BitmapIconResizer.kt
@@ -9,9 +9,10 @@ object BitmapIconResizer : IconResizer {
 
     override fun resize(bytes: ByteArray, width: Int, height: Int): ByteArray {
         val baos = ByteArrayOutputStream()
-        BitmapFactory.decodeByteArray(bytes, 0, bytes.size).apply {
-            reconfigure(width, height, Bitmap.Config.ARGB_8888)
-        }.compress(Bitmap.CompressFormat.PNG, 100, baos)
+        BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+            .copy(Bitmap.Config.ARGB_8888, true).apply {
+                reconfigure(width, height, Bitmap.Config.ARGB_8888)
+            }.compress(Bitmap.CompressFormat.PNG, 100, baos)
         return baos.toByteArray()
     }
 }
