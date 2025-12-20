3efd1a382ac0df5b624fc86e2133f353af1d78fe
Mathew Tkachuk <me@crx.moe>
2025-12-20 04:15:43 +0300

    Remove unused VrApi after patching

--- overportcli/src/main/java/moe/crx/overport/patches/CopyOVRPluginVrApiPatch.kt
+++ overportcli/src/main/java/moe/crx/overport/patches/CopyOVRPluginVrApiPatch.kt
@@ -9,11 +9,12 @@ val PATCH_COPY_OVRPLUGIN_VRAPI = Patch("patch_copy_ovrplugin_vrapi") {
             val arch = getLibraries().resolve("lib/${archDirectory.name}")
 
             if (arch.exists()) {
-                val hasVrApi = archDirectory.listFiles().find { it.name.lowercase() == "libvrapi.so" } != null
-                if (hasVrApi) {
+                val hasVrApi = archDirectory.listFiles().find { it.name.lowercase() == "libvrapi.so" }
+                if (hasVrApi != null) {
                     arch.listFiles().filter { it.name == "libOVRPlugin.so" }.forEach { library ->
                         library.copyTo(File(archDirectory, library.name), true)
                     }
+                    hasVrApi.delete()
                 }
             }
         }
