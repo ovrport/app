508fb2e3f590db696d5960ef2ad2ae0cc7cd73b3
Mathew Tkachuk <me@crx.moe>
2025-12-20 04:14:40 +0300

    Create Unity stub if UE4 smali is present only

--- overportcli/src/main/java/moe/crx/overport/patches/FixUnrealCrashPatch.kt
+++ overportcli/src/main/java/moe/crx/overport/patches/FixUnrealCrashPatch.kt
@@ -3,18 +3,18 @@ package moe.crx.overport.patches
 import moe.crx.overport.patching.Patch
 
 val PATCH_FIX_UNREAL_CRASH = Patch("patch_fix_unreal_crash") {
-    createSmali("com/unity3d/player/UnityPlayer") {
-        if (readText().isBlank()) {
-            append(".class public Lcom/unity3d/player/UnityPlayer;")
-            append(".super Ljava/lang/Object;")
-            append(".field public static currentActivity:Landroid/app/Activity;")
-        }
-    }
-
     selectSmali("com/epicgames/ue4/GameActivity") {
         replace(
             "sput-object (\\w+), Lcom\\/epicgames\\/ue4\\/GameActivity;->\\w+:Lcom\\/epicgames\\/ue4\\/GameActivity;",
             "$0\nsput-object $1, Lcom/unity3d/player/UnityPlayer;->currentActivity:Landroid/app/Activity;"
         )
+        
+        createSmali("com/unity3d/player/UnityPlayer") {
+            if (readText().isBlank()) {
+                append(".class public Lcom/unity3d/player/UnityPlayer;")
+                append(".super Ljava/lang/Object;")
+                append(".field public static currentActivity:Landroid/app/Activity;")
+            }
+        }
     }
 }
