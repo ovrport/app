c09de5b893af67de862d72c57a7820dc590f61dc
Mathew Tkachuk <me@crx.moe>
2025-12-21 21:18:10 +0300

    Android XR metadata

--- overportcli/src/main/java/moe/crx/overport/patches/VRMetadataPatch.kt
+++ overportcli/src/main/java/moe/crx/overport/patches/VRMetadataPatch.kt
@@ -22,10 +22,63 @@ fun createMetadata(name: String, value: String): JSONObject {
     )
 }
 
+fun createUsesFeature(name: String): JSONObject {
+    return JSONObject().put("node_type", "element").put("name", "uses-feature").put(
+        "attributes", JSONArray().put(
+            JSONObject().put("name", "name").put("id", 16842755)
+                .put("uri", "http://schemas.android.com/apk/res/android").put("prefix", "android")
+                .put("value_type", "STRING").put("data", name)
+        ).put(
+            JSONObject().put("name", "required").put("id", 16843406)
+                .put("uri", "http://schemas.android.com/apk/res/android").put("prefix", "android")
+                .put("value_type", "BOOLEAN").put("data", true)
+        )
+    )
+}
+
+fun createUsesPermission(name: String): JSONObject {
+    return JSONObject().put("node_type", "element").put("name", "uses-permission").put(
+        "attributes", JSONArray().put(
+            JSONObject().put("name", "name").put("id", 16842755)
+                .put("uri", "http://schemas.android.com/apk/res/android").put("prefix", "android")
+                .put("value_type", "STRING").put("data", name)
+        )
+    )
+}
+
+fun createProperty(key: String, value: String): JSONObject {
+    return JSONObject().put("node_type", "element").put("name", "property").put(
+        "attributes", JSONArray().put(
+            JSONObject().put("name", "name").put("id", 16842755)
+                .put("uri", "http://schemas.android.com/apk/res/android").put("prefix", "android")
+                .put("value_type", "STRING").put("data", key)
+        ).put(
+            JSONObject().put("name", "value").put("id", 16842788)
+                .put("uri", "http://schemas.android.com/apk/res/android").put("prefix", "android")
+                .put("value_type", "STRING").put("data", value)
+        )
+    )
+}
+
 val PATCH_VR_METADATA = Patch("patch_vr_metadata") {
     selectManifestJson {
         takeNodesEach({ named("manifest") }) {
             takeNodesEach({ named("application") }) {
+                takeNodesEach({ named("activity") }) {
+                    takeNodes {
+                        this?.put(
+                            createProperty(
+                                "android.window.PROPERTY_XR_ACTIVITY_START_MODE",
+                                "XR_ACTIVITY_START_MODE_FULL_SPACE_UNMANAGED"
+                            )
+                        )
+                    }
+                }
+                takeNodes {
+                    this?.put(createUsesFeature("android.software.xr.api.openxr"))
+                        ?.put(createUsesPermission("org.khronos.openxr.permission.OPENXR"))
+                        ?.put(createUsesPermission("org.khronos.openxr.permission.OPENXR_SYSTEM"))
+                }
                 takeNodesEach({ named("meta-data") }) {
                     if (nameAttribute() == "com.oculus.supportedDevices") null else this
                 }
