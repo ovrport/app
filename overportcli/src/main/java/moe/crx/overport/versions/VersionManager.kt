994bf1a4901244d9faec10e5aac47de09baf56ca
Mathew Tkachuk <me@crx.moe>
2025-12-21 21:50:09 +0300

    Bump version

--- overportcli/src/main/java/moe/crx/overport/versions/VersionManager.kt
+++ overportcli/src/main/java/moe/crx/overport/versions/VersionManager.kt
@@ -10,7 +10,7 @@ class VersionManager(val workingDirectory: File) {
 
     companion object {
         val json = Json { ignoreUnknownKeys = true }
-        const val VERSION = "1.2.1"
+        const val VERSION = "1.2.2"
 
         private fun isVersionLower(left: String, right: String): Boolean {
             val (lY, lM, lD) = left.substringBeforeLast('-').split('.').map { it.toInt() }
